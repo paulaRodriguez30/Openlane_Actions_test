8b375e14239e305c2054c088edf9efc1cc78096c
paulaRodriguez30 <150388861+paulaRodriguez30@users.noreply.github.com>
2023-11-09 15:30:54 -0500

    Add files via upload

--- src/src.v
+++ src/src.v
@@ -1,60 +1,95 @@
-module src(
-    input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
-    output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
-    input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-    output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-    output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to 
-    
-    );
-    
-    wire [2:0] bits ;
+module src (
+    input  wire [7:0] ui_in,  // Input numbers
+    output reg [7:0] uo_out,   //Output number
+    input  wire [7:0] uio_in,   //Control ALU / Select input number
+    output wire [7:0] uio_out,  
+    output wire [7:0] uio_oe,   
+    input  wire       ena,      
+    input  wire       clk,     //Clock
+    input  wire       rst_n     // Res
+);
 
-    reg [7:0] cnt;
-    wire [7:0] duty;
-    reg pwm_q,ppm_q;
-    reg [2:0] bits_pre;
-    wire pwm_d,ppm_d;
+reg [7:0] A, B;
+reg flag, overflow;
+reg [5:0] out;
 
-    assign duty = ui_in [7:0];
-    assign bits = uio_in[2:0];
+wire [2:0] ALUcontrol;
+wire enA, enB;
+wire [1:0] flagcontrol;
 
-    assign uio_oe = 8'b11111000;
-    assign uio_out[7:0] = 8'b00000000;
-    assign uo_out[7:1] = 7'b0000000;
-    
-    always @(posedge clk) begin
-      if(rst_n) begin
-        bits_pre <= bits;
-        if(bits_pre != bits) begin
-            cnt <= 8'd0;
-            pwm_q <= 1'b0;
-        end
-        else begin
-            
-            pwm_q <= pwm_d;
-                     
-            if((cnt >= (2**bits)))
-                cnt <= 0;
-            else begin     
-                cnt <= cnt + 1;
-            end
-        end
-        bits_pre <= bits;
-      end else begin
-         pwm_q <= 1'b0;
-         cnt <= 0;
-         bits_pre <= bits;
-      end
+assign ALUcontrol = uio_in[2:0];
+assign enA = uio_in[3];
+assign flagcontrol = uio_in[5:4];
+
+assign uio_out[5:0] = out;
+assign uio_out[6] = flag;
+assign uio_out[7] = overflow;
+
+assign uio_oe = 8'b11111111;
+
+assign enB = ~enA;
+
+
+// ALU control
+always @(*) begin
+    case (ALUcontrol)
+        3'b000: {out[5:0], uo_out} = A + B;                 // Adition
+        3'b001: {out[5:0], uo_out} = A - B;                 // Substraction
+        3'b010: {out[5:0], uo_out} = {1'b0, A[7:1]};           // Shift right
+        3'b011: {out[5:0], uo_out} = {A[6:0], 1'b0};           // Shift left
+        3'b100: {out[5:0], uo_out} = A & B;                 // AND
+        3'b101: {out[5:0], uo_out} = A | B;                 // OR
+        3'b110: {out[5:0], uo_out} = A ^ B;                 //XOR
+        3'b111: {out[5:0], uo_out} = A * B; //Mult
+        default: {out[5:0], uo_out} = 14'd0;
+    endcase
+end
+
+
+// Flag Control
+always @(*) begin
+    case (flagcontrol)
+        2'b00: flag = A > B;        // Greater than 
+        2'b01: flag = A == B;       // Equal
+        2'b10: flag = A == 8'd0;    // Equal cero
+        2'b11: flag = A[0] == 0;    // Check even
+        default: flag = 0;
+    endcase
+end
+
+// Overflow
+always @(*) begin
+    if(uio_out[5:0] == 6'd0) begin
+        overflow <= 0;
+    end       
+    else begin
+        overflow <= 1;
     end
+end
 
+// Input registers
+
+// register A
+always @( posedge clk) begin
+    if (enA) 
+        A <= ui_in;
+end
+// Resgistrer B
+always @( posedge clk) begin
+    if (enB) 
+        B <= ui_in;
+end
+
+// Reset
+always @(*) begin
+    if (~rst_n)
+        A <= 8'd0;
+        B <= 8'd0;
+        uo_out <= 8'd0;
+        out <= 6'd0;
+        overflow <= 0;
+        flag <= 0;
+
+end
     
-    assign pwm_d = (cnt < duty);
-    assign uo_out[0] = pwm_q;
-    
-
-
 endmodule
-
